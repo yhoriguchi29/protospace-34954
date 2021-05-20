ab4da83e58b8e4590c5067808403ad043433a8d1
HOYU <yuhi.horiguchi@gmail.com>
2021-05-20 15:26:43 +0900

    空の入力欄がある場合は、編集できずにそのページに留まること & バリデーションによって投稿ができず、そのページに留まった場合でも、入力済みの項目（画像以外）は消えないこと

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,5 +1,5 @@
 class PrototypesController < ApplicationController
-  before_action :move_to_index, except: [:index, :show]
+  before_action :move_to_index, only: [:edit]
 
   def index
     @prototypes = Prototype.all
